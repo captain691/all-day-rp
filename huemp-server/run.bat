8a3c3eab72f88c13709eb9fdeee548e100cdae60
captain691 <86930651+captain691@users.noreply.github.com>
2021-07-15 18:51:31 -0500

    !-- ----------------------- [ MenuV ] ----------------------- -- GitHub: https://github.com/ThymonA/menuv/ -- License: GNU General Public License v3.0 --          https://choosealicense.com/licenses/gpl-3.0/ -- Author: Thymon Arens <contact@arens.io> -- Name: MenuV -- Version: 1.0.0 -- Description: FiveM menu library for creating menu's ----------------------- [ MenuV ] ----------------------- --> <!doctype html> <html class="no-js" lang="">     <head>         <meta charset="utf-8">         <title>MenuV</title>         <meta name="description" content="">         <meta name="viewport" content="width=device-width, initial-scale=1">         <meta http-equiv="cache-control" content="max-age=0" />         <meta http-equiv="cache-control" content="no-cache" />         <meta http-equiv="expires" content="0" />         <meta http-equiv="pragma" content="no-cache" />         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin="anonymous" />         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" crossorigin="anonymous" />         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" crossorigin="anonymous" />         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" crossorigin="anonymous" />         <link rel="stylesheet" href="./assets/css/main.css">         <link rel="stylesheet" href="./assets/css/native_theme.css">     </head>     <body>         <div id="menuv"></div>         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" crossorigin="anonymous"></script>     </body> </html>
    !--
    ----------------------- [ MenuV ] -----------------------
    -- GitHub: https://github.com/ThymonA/menuv/
    -- License: GNU General Public License v3.0
    --          https://choosealicense.com/licenses/gpl-3.0/
    -- Author: Thymon Arens <contact@arens.io>
    -- Name: MenuV
    -- Version: 1.0.0
    -- Description: FiveM menu library for creating menu's
    ----------------------- [ MenuV ] -----------------------
    -->
    <!doctype html>
    <html class="no-js" lang="">
        <head>
            <meta charset="utf-8">
            <title>MenuV</title>
            <meta name="description" content="">
            <meta name="viewport" content="width=device-width, initial-scale=1">
            <meta http-equiv="cache-control" content="max-age=0" />
            <meta http-equiv="cache-control" content="no-cache" />
            <meta http-equiv="expires" content="0" />
            <meta http-equiv="pragma" content="no-cache" />
            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin="anonymous" />
            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" crossorigin="anonymous" />
            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" crossorigin="anonymous" />
            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" crossorigin="anonymous" />
            <link rel="stylesheet" href="./assets/css/main.css">
            <link rel="stylesheet" href="./assets/css/native_theme.css">
        </head>
        <body>
            <div id="menuv"></div>
            <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" crossorigin="anonymous"></script>
        </body>
    </html>

---
+++ huemp-server/run.bat
@@ -0,0 +1,2 @@
+cd /d C:\Users\foxra\OneDrive\Documents\GitHub\all-day-rp\huemp-server\cfx-server-data-master
+C:\Users\foxra\OneDrive\Documents\GitHub\all-day-rp\huemp-server\FXServer.exe +exec server.cfg
